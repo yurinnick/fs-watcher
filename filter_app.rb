549eb85ac1110e87d20d8f19ab335b8d8c265fe5
Nikolay Yurin <nyurin@griddynamics.com>
2014-07-27 04:25:04 +0400

    Fix median calculation for an emplty filelist

--- filter_app.rb
+++ filter_app.rb
@@ -14,9 +14,13 @@ class FileFilterApp < Sinatra::Base
     file_list = Dir.glob('/home/**/*')
     .select { |path| File.ctime(path) > (Time.now - params[:sec].to_i) }
 
-    sorted = file_list.map { |f| File.basename(f).length }.sort
-    len = sorted.length
-    median = (sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0
+    median =  if file_list.empty?
+                0
+              else
+                sorted = file_list.map { |f| File.basename(f).length }.sort
+                len = sorted.length
+                (sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0
+              end
     return { "files" => file_list, "median_length" => median }.to_json
   end
 end
